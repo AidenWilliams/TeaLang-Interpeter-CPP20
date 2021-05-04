fd9761028936c0a684374b2e28b5d2e694272edf
Aiden <mw3dbest@gmail.com>
2021-05-04 18:01:21 +0200

    Added parsing statement definitions

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -5,10 +5,40 @@
 #ifndef TEALANG_COMPILER_CPP20_PARSER_H
 #define TEALANG_COMPILER_CPP20_PARSER_H
 
+#include <utility>
+#include <vector>
+#include <string>
+#include "../Visitor/Visitor.h"
+#include "../Lexer/Lexer.h"
+#include "../Lexer/Token.h"
+#include "AST.h"
 
-class Parser {
+namespace parser {
+    class Parser {
+    public:
+        Parser(std::vector<lexer::Token> tokens) :
+                tokens(std::move(tokens))
+        {};
+        ASTProgramNode* parseProgram();
 
-};
+    private:
+        std::vector<lexer::Token> tokens;
+        std::vector<lexer::Token>::iterator currentToken;
+        std::vector<lexer::Token>::iterator nextToken;
+
+        // Statement Nodes
+        ASTStatementNode* parseStatement();
+        ASTAssignmentNode* parseAssignment();
+        ASTDeclarationNode* parseDeclaration();
+        ASTPrintNode* parsePrint();
+        ASTReturnNode* parseReturn();
+        ASTBlockNode* parseBlock();
+        ASTIfNode* parseIf();
+        ASTForNode* parseFor();
+        ASTWhileNode* parseWhile();
+        ASTFunctionDefinitionNode* parseFunctionDefinition();
+    };
+}
 
 
 #endif //TEALANG_COMPILER_CPP20_PARSER_H
