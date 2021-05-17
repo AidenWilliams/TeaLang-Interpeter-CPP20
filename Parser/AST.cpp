1703c4c5c8bc10c049723d490e21a3822ff3346b
Aiden <mw3dbest@gmail.com>
2021-05-17 18:34:13 +0200

    removed ast.cpp

--- Parser/AST.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Aiden Williams on 03/05/2021.
-//
-
-#include "AST.h"
-
-//parser::ASTProgramNode::ASTProgramNode(std::vector<ASTStatementNode *> statements) :
-//    statements(std::move(statements))
-//{}
