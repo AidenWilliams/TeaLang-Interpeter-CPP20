d617a471775d1db25a57d6adba88c0ce80734456
Aiden <mw3dbest@gmail.com>
2021-05-17 15:14:44 +0200

    Restarting parser

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -5,56 +5,5 @@
 #ifndef TEALANG_COMPILER_CPP20_PARSER_H
 #define TEALANG_COMPILER_CPP20_PARSER_H
 
-#include <vector>
-#include <string>
-#include <stdexcept>
-#include "../Visitor/Visitor.h"
-#include "../Lexer/Lexer.h"
-#include "../Lexer/Token.h"
-#include "AST.h"
-
-namespace parser {
-    class Parser {
-    public:
-        Parser(std::vector<lexer::Token> tokens)
-        {
-            currentToken = tokens.begin();
-            nextToken = currentToken + 1;
-        };
-        ASTProgramNode* parseProgram();
-
-    private:
-        void moveTokenWindow(int step=1);
-        std::vector<lexer::Token>::iterator currentToken;
-        std::vector<lexer::Token>::iterator nextToken;
-
-        bool verifyFoundFactor();
-
-        bool foundAssignment();
-        bool foundLiteral();
-        bool foundFunctionCall();
-        bool foundIdentifier();
-        bool foundMultiplicativeOp();
-        bool foundAdditiveOp();
-        bool foundRelationalOp();
-        bool foundSemicolon();
-
-        // Expression Nodes
-        ASTExprNode* parseExpression();
-
-        // Statement Nodes
-        ASTStatementNode* parseStatement();
-        ASTAssignmentNode* parseAssignment(const std::string& identifier);
-        ASTDeclarationNode* parseDeclaration();
-        ASTPrintNode* parsePrint();
-        ASTReturnNode* parseReturn();
-        ASTBlockNode* parseBlock();
-        ASTIfNode* parseIf();
-        ASTForNode* parseFor();
-        ASTWhileNode* parseWhile();
-        ASTFunctionDefinitionNode* parseFunctionDefinition();
-    };
-}
-
 
 #endif //TEALANG_COMPILER_CPP20_PARSER_H
