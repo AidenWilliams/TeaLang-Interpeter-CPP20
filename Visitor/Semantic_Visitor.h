875274032e18ef6211d5d84657ebb902c08726d7
Aiden <mw3dbest@gmail.com>
2021-06-26 16:23:39 +0200

    variable, function and scope classes

--- Visitor/Semantic_Visitor.h
+++ Visitor/Semantic_Visitor.h
@@ -7,12 +7,61 @@
 
 #include "Visitor.h"
 #include "../Parser/AST.h"
+#include <utility>
+#include <vector>
+#include <map>
 
 namespace visitor {
+    class variable{
+    public:
+//        lookup(std::vector<>, std::string identifier);
+    private:
+        variable(std::string identifier, parser::TYPE type, unsigned int lineNumber) :
+                identifier(std::move(identifier)),
+                type(type),
+                lineNumber(lineNumber)
+        {};
+        ~variable() = default;
+
+        std::string identifier;
+        parser::TYPE type;
+        unsigned int lineNumber;
+    };
+
+    class function{
+//        lookup(std::vector<>, std::string identifier);
+    private:
+        function(std::string identifier, parser::TYPE type, std::vector<parser::TYPE> paramTypes, unsigned int lineNumber) :
+                identifier(std::move(identifier)),
+                type(type),
+                paramTypes(std::move(paramTypes)),
+                lineNumber(lineNumber)
+        {};
+        ~function() = default;
+
+        std::string identifier;
+        parser::TYPE type;
+        std::vector<parser::TYPE> paramTypes;
+        unsigned int lineNumber;
+    };
+
+    class SemanticScope {
+    public:
+
+    private:
+        // Python equivalent of:
+        // variableTable = {identifier: {identifier, TYPE, lineNumber}}
+        std::map<std::string, variable> variableTable;
+        // Python equivalent of:
+        // functionTable = {identifier: {identifier, FUNCTION_TYPE, [ARGUMENT_TYPES,], lineNumber}}
+        std::map<std::string, function> functionTable;
+    };
+
+
     class SemanticAnalyser : public Visitor {
     public:
         SemanticAnalyser();
-        ~SemanticAnalyser();
+        ~SemanticAnalyser() = default;
 
         void visit(parser::ASTProgramNode* programNode) override;
 
