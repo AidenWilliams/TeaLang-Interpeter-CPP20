31e0dda52359657c3c52c7db6be10186314ec3a8
Aiden <mw3dbest@gmail.com>
2021-06-26 15:55:49 +0200

    Semantic_Visitor.h visitor build

--- Visitor/Semantic_Visitor.h
+++ Visitor/Semantic_Visitor.h
@@ -5,10 +5,39 @@
 #ifndef TEALANG_COMPILER_CPP20_SEMANTIC_VISITOR_H
 #define TEALANG_COMPILER_CPP20_SEMANTIC_VISITOR_H
 
+#include "Visitor.h"
+#include "../Parser/AST.h"
 
-class Semantic_Visitor {
+namespace visitor {
+    class SemanticAnalyser : public Visitor {
+    public:
+        SemanticAnalyser();
+        ~SemanticAnalyser();
 
-};
+        void visit(parser::ASTProgramNode* programNode) override;
+
+        void visit(parser::ASTLiteralNode<int>* literalNode) override;
+        void visit(parser::ASTLiteralNode<float>* literalNode) override;
+        void visit(parser::ASTLiteralNode<bool>* literalNode) override;
+        void visit(parser::ASTLiteralNode<std::string>* literalNode) override;
+        void visit(parser::ASTBinaryNode* binaryNode) override;
+        void visit(parser::ASTIdentifierNode* identifierNode) override;
+        void visit(parser::ASTUnaryNode* unaryNode) override;
+        void visit(parser::ASTFunctionCallNode* functionCallNode) override;
+
+        void visit(parser::ASTSFunctionCallNode* sFunctionCallNode) override;
+        void visit(parser::ASTDeclarationNode* declarationNode) override;
+        void visit(parser::ASTAssignmentNode* assignmentNode) override;
+        void visit(parser::ASTPrintNode* printNode) override;
+        void visit(parser::ASTBlockNode* blockNode) override;
+        void visit(parser::ASTIfNode* ifNode) override;
+        void visit(parser::ASTForNode* forNode) override;
+        void visit(parser::ASTWhileNode* whileNode) override;
+        void visit(parser::ASTFunctionDeclarationNode* functionDeclarationNode) override;
+        void visit(parser::ASTReturnNode* returnNode) override;
+    };
+}
+
 
 
 #endif //TEALANG_COMPILER_CPP20_SEMANTIC_VISITOR_H
